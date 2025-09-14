488ce4d771350deddc2debd250ef0da2bc22e391
notkilab <79872728+kilabgaming@users.noreply.github.com>
2025-09-14 16:44:06 +0200

    start containers in foreground

--- scripts/docker-start.bat
+++ scripts/docker-start.bat
@@ -6,13 +6,4 @@ cd ..
 
 echo Starting Auton8 stack...
 
-:: starting containers in background
-docker compose up -d
-
-echo.
-echo Auton8 services are now running.
-echo - Mosquitto on 127.0.0.1:1883
-echo - n8n editor on http://localhost:5678
-echo - Read live logs with: docker compose logs -f
-
-pause
+docker compose up
